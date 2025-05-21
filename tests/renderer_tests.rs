828928608b27a6b27e26689fba14912efcc89d16
Konstantinos Athanasiou <dinosath0@gmail.com>
2025-05-21 22:54:05 +0300

    add test for loop example

--- tests/renderer_tests.rs
+++ tests/renderer_tests.rs
@@ -98,3 +98,20 @@ fn test_demo_copy() {
     assert!(!dir_diff::is_different(tmp_dir.path().to_path_buf(), "tests/expected/demo")
         .unwrap());
 }
+
+#[test]
+fn test_loop_example() {
+    let tmp_dir = tempfile::tempdir().unwrap();
+    let args = Args {
+        template: "examples/loop".to_string(),
+        output_dir: tmp_dir.path().to_path_buf(),
+        force: true,
+        verbose: false,
+        answers: Some("{\"items\":[{\"name\":\"item1\"},{\"name\":\"item2\"}]}".to_string()),
+        skip_confirms: vec![All],
+        non_interactive: true,
+    };
+    run(args).unwrap();
+    assert!(!dir_diff::is_different(tmp_dir.path().to_path_buf(), "tests/expected/loop")
+        .unwrap());
+}
