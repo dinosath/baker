ff9dabdf65b2bc209dbdc232b4e1e2b6052a6c38
Konstantinos Athanasiou <dinosath0@gmail.com>
2025-07-22 20:48:48 +0300

    fmt

--- tests/renderer_tests.rs
+++ tests/renderer_tests.rs
@@ -109,7 +109,9 @@ mod tests {
         run_and_assert(
             "examples/loop",
             "tests/expected/loop",
-            Some("{\"nested\":true,\"items\":[{\"name\":\"item1\"},{\"name\":\"item2\"}]}"),
+            Some(
+                "{\"nested\":true,\"items\":[{\"name\":\"item1\"},{\"name\":\"item2\"}]}",
+            ),
         );
     }
 }
